81f42ab29adb201e635303d833f46f3441fbb8c9
oleg shepotkin <oleg.shepotkin@artezio.com>
2018-01-03 21:21:35 +0300

    init commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+
+import { AppComponent } from './app.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
